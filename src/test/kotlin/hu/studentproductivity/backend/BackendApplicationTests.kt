2111ee50a95a86c39c0e5c96f9e01f131ecdc66d
Pozsonyi Balázs Dávid <balazs.pozsonyi@barre.hu>
2025-11-18 14:18:55 +0100

    Initial commit - spring boot kotlin backend

---
+++ src/test/kotlin/hu/studentproductivity/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.studentproductivity.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
